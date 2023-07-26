e0d7e6a84bef794479615a75497e7d1fc2d06a83
Tijani Femi <olutimedia@gmail.com>
2023-07-26 18:16:31 +0100

    updated login task

---
+++ src/database/banner.list.ts
@@ -0,0 +1,65 @@
+const bannersList = [
+  {
+    image:
+      'https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-84927.jpg',
+    description:
+      'Kick back with our vibrant new Frozen Pineapple Passionfruit Lemonade Starbucks Refreshers® beverage.',
+    _id: '141e5912-9d38-40da-ad44-a6aa4feecf48',
+    btn_text: 'Order now',
+    color: '#1e3932',
+    title: 'Tropical cooldown',
+    bg_color: '#eb81a5',
+    btn_url: '/',
+  },
+  {
+    image:
+      'https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-84954.jpg',
+    description:
+      'WinsDays just got even cooler for Starbucks® Rewards members. Now enjoy 50% off a cold drink on Wednesdays after 12 p.m.*',
+    _id: 'f86bd8d0-4c24-40f8-8bdf-f21ec16dea6a',
+    btn_text: 'Join for free',
+    color: '#006241',
+    title: 'Now 50% off cold drinks',
+    bg_color: '#e5ff67',
+    btn_url: '/',
+  },
+  {
+    image:
+      'https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-84930.jpg',
+    description:
+      'Go for a Mocha Cookie Crumble or Caramel Ribbon Crunch Frappuccino® blended beverage.',
+    _id: '485ca018-63e0-4148-8302-4c9f7f15eb84',
+    btn_text: 'Order now',
+    color: '#000000',
+    title: 'Summer to the max',
+    bg_color: '#8f8bf4',
+    btn_url: '/',
+  },
+  {
+    image:
+      'https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-84933.jpg',
+    description:
+      'This adorable Bumblebee Cake Pop dipped in chocolatey icing has landed',
+    _id: '50d78560-a927-4cc9-a819-9b2305bd77f6',
+    btn_text: 'Order now',
+    color: '#1e3932',
+    title: 'Follow the buzz',
+    bg_color: '#f1ff67',
+    btn_url: '/',
+  },
+
+  {
+    image:
+      'https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-84617.jpg',
+    description:
+      'We teamed up with Headspace to bring you four free meditations on kindness, plus a special offer for Starbucks® Rewards members—discover more mindfulness with a two-month free membership.**',
+    _id: '7eb58ada-99f5-4da7-aae4-feeef8964227',
+    btn_text: 'Listen now',
+    color: '#1e3932',
+    title: 'Be kind to your mind',
+    bg_color: '#e1fccd',
+    btn_url: '/',
+  },
+];
+
+export default bannersList;
